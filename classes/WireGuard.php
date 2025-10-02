acd6dcfa50c9d1bebe2648d4d754172c0cd53702
Alvin Kiveu <67068458+alvin-kiveu@users.noreply.github.com>
2025-10-02 15:20:44 +0300

    new edata

--- classes/WireGuard.php
+++ classes/WireGuard.php
@@ -242,18 +242,13 @@ class WireGuard {
     public function getPeers($activeOnly = true) {
         // Get current interface name without wg_ prefix for database lookup
         $interface_name = preg_replace('/^wg_/', '', $this->interfaceName);
-        //get iface_id from interfaces table
-        $iface_id = $this->db->selectOne("SELECT id FROM interfaces WHERE name = ?", [$interface_name]);
-        if (!$iface_id) {
-            throw new \Exception("Interface not found");
-        }
-
+        
         $sql = "SELECT * FROM wg_peers WHERE iface_id = ?";
-        $params = [$iface_id];
-
-        // if ($activeOnly) {
-        //     $sql .= " AND status = 'active'";
-        // }
+        $params = [$interface_name];
+        
+        if ($activeOnly) {
+            $sql .= " AND status = 'active'";
+        }
         $sql .= " ORDER BY created_at DESC";
         
         try {
